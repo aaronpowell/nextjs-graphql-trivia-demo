6cc706ba0c62ffaf7f28b25903caa890869d81af
Aaron Powell <me@aaron-powell.com>
2022-04-29 10:47:02 +1000

    meeting required name rules

--- cosmos.ts
+++ cosmos.ts
@@ -1,11 +1,11 @@
 import { CosmosClient } from "@azure/cosmos";
 
-const endpoint = process.env.ENDPOINT || "https://localhost:8081";
+const endpoint = process.env.COSMOS_ENDPOINT || "https://localhost:8081";
 const key =
-  process.env.KEY ||
+  process.env.COSMOS_KEY ||
   "C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==";
-const databaseId = process.env.DATABASE_ID || "graphql";
-const containerId = process.env.CONTAINER_ID || "graphql";
+const databaseId = process.env.COSMOS_DATABASE_ID || "graphql";
+const containerId = process.env.COSMOS_CONTAINER_ID || "graphql";
 const client = new CosmosClient({
   endpoint,
   key,
