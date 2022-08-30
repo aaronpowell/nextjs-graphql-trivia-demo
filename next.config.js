2dad85ad850294cc2a900be0a704a3b728b402a9
Aaron Powell <me@aaron-powell.com>
2022-08-30 02:11:05 +0000

    more languages and a language switcher

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   i18n: {
-    locales: ["en", "fr", "nl"],
+    locales: ["en", "fr", "nl", "de", "es", "it", "pt"],
     defaultLocale: "en",
   },
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,7 @@ import Head from "next/head";
 import styles from "../styles/Home.module.css";
 import { container } from "../cosmos";
 import { Question } from "../components/Question";
+import Navbar from "./components/Navbar";
 
 export async function getServerSideProps() {
   const querySpec = {
@@ -22,41 +23,48 @@ export async function getServerSideProps() {
 
 const Home: NextPage<{ count: number }> = (props) => {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Test your trivia skills</title>
-        <meta name="description" content="An awesome trivia app" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <>
+      <Navbar />
+      <div className={styles.container}>
+        <Head>
+          <title>Test your trivia skills</title>
+          <meta name="description" content="An awesome trivia app" />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>Trivia time!</h1>
+        <main className={styles.main}>
+          <h1 className={styles.title}>Trivia time!</h1>
 
-        <Question count={props.count} />
-      </main>
+          <Question count={props.count} />
+        </main>
 
-      <footer className={styles.footer}>
-        <a href="https://nextjs.org/" target="_blank" rel="noopener noreferrer">
-          Build with Next.js
-        </a>
+        <footer className={styles.footer}>
+          <a
+            href="https://nextjs.org/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Build with Next.js
+          </a>
 
-        <a
-          href="https://docs.microsoft.com/azure/static-web-apps/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by Azure
-        </a>
+          <a
+            href="https://docs.microsoft.com/azure/static-web-apps/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Powered by Azure
+          </a>
 
-        <a
-          href="https://github.com/aaronpowell/nextjs-graphql-trivia-demo"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Open Sourced on GitHub
-        </a>
-      </footer>
-    </div>
+          <a
+            href="https://github.com/aaronpowell/nextjs-graphql-trivia-demo"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Open Sourced on GitHub
+          </a>
+        </footer>
+      </div>
+    </>
   );
 };
 
