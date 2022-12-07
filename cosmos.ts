d809a41ec719f0bd5e7c0580dd7091850c9bccb3
Dina Berry (SHE/HER) <Dina.Berry@microsoft.com>
2022-12-06 17:21:49 -0800

    remove keys

--- cosmos.ts
+++ cosmos.ts
@@ -1,9 +1,8 @@
 import { CosmosClient } from "@azure/cosmos";
 
-const endpoint = process.env.COSMOS_ENDPOINT || "https://diberry-cosmosdb-nosql.documents.azure.com:443/" || "https://localhost:8081";
+const endpoint = process.env.COSMOS_ENDPOINT || "https://.documents.azure.com:443/" || "https://localhost:8081";
 const key =
-  process.env.COSMOS_KEY ||
-  "m9XZd9qvF2BJKFcTCXYIz6NEcKCLJsMAHCZ8S1agel38oZzwJPvHBbrjnOuONaC3ekQ8TKtzYH6fACDbfn3kVw==";
+  process.env.COSMOS_KEY;
 const databaseId = process.env.COSMOS_DATABASE_ID || "trivia";
 const containerId = process.env.COSMOS_CONTAINER_ID || "questions";
 const client = new CosmosClient({
--- utils/bootstrap.mjs
+++ utils/bootstrap.mjs
@@ -4,10 +4,9 @@ import { readFile } from "fs/promises"
 
 config();
 
-const endpoint = process.env.COSMOS_ENDPOINT || "https://diberry-cosmosdb-nosql.documents.azure.com:443/" || "https://localhost:8081";
+const endpoint = process.env.COSMOS_ENDPOINT || "https://NOSQL-COSMOSDB-RESOURCE-NAME.documents.azure.com:443/" || "https://localhost:8081";
 const key =
-  process.env.COSMOS_KEY ||
-  "C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==";
+  process.env.COSMOS_KEY;
 const databaseId = process.env.COSMOS_DATABASE_ID || "graphql";
 const containerId = process.env.COSMOS_CONTAINER_ID || "graphql";
 const client = new CosmosClient({
