48c119c17330242d1eddc783ce7c49ece735bb60
Dina Berry (SHE/HER) <Dina.Berry@microsoft.com>
2022-12-14 06:42:38 -0800

    working with Aaron

--- pages/api/resolvers/resolvers.ts
+++ pages/api/resolvers/resolvers.ts
@@ -1,6 +1,12 @@
 import { SqlQuerySpec } from "@azure/cosmos";
 import type { ApolloContext } from "../context/ApolloContext";
 
+type AnswerCheckModel = {
+  questionId: string
+  correct: boolean
+  correctAnswer: string
+}
+
 const arrayRandomiser = <T>(array: T[]) =>
   array.sort(() => 0.5 - Math.random());
 
@@ -31,12 +37,20 @@ export const resolvers = {
       return question;
     },
   },
-  Question: {
+  Question: { //overwrite field resolver
+    correctAnswer(question: any){
+      return question.correct_answer;
+    },
     answers(question: any) {
       return arrayRandomiser(
         question.incorrect_answers.concat(question.correct_answer)
       );
     },
+  },
+  AnswerCheck: {
+    question(answerCheck: AnswerCheckModel, _: null, { dataSources }: ApolloContext) {
+      return dataSources.questions.findOneById(answerCheck.questionId);
+    }
   },
   Mutation: {
     async validateAnswer(
